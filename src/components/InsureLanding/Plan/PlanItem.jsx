2864bfcc624851b5426755dc51c5ce1e98e30a24
siavhnz <siavhnz@gmail.com>
2023-06-27 23:09:25 +0330

    fix images alt

--- src/components/InsureLanding/Plan/PlanItem.jsx
+++ src/components/InsureLanding/Plan/PlanItem.jsx
@@ -1,16 +1,11 @@
 const PlanItem = ({ title, desc, icon }) => {
   return (
-    <div className="flex flex-col items-center 1.5lg:items-start 1.5lg:flex-1">
-      <img
-        src={icon}
-        aria-hidden={true}
-        focusable={false}
-        className="h-86 w-86"
-      />
+    <div className="flex flex-col items-center 1.5lg:flex-1 1.5lg:items-start">
+      <img src={icon} alt={title} className="h-86 w-86" />
       <h3 className="mt-35 font-dm-serif-display text-27 leading-32 text-very-dark-violet">
         {title}
       </h3>
-      <p className="mt-19 px-26 text-center 1.5lg:text-left 1.5lg:px-0 font-Karla text-16 leading-25 text-dark-grayish-violet">
+      <p className="mt-19 px-26 text-center font-Karla text-16 leading-25 text-dark-grayish-violet 1.5lg:px-0 1.5lg:text-left">
         {desc}
       </p>
     </div>
