032acb26b73f82ccd73fefe5f21f1fd6c8218115
siavhnz <siavhnz@gmail.com>
2023-06-27 23:01:15 +0330

    fix PlanItem reference name

--- src/components/InsureLanding/Plan/Plans.jsx
+++ src/components/InsureLanding/Plan/Plans.jsx
@@ -1,9 +1,9 @@
 import { plans } from "../../../store/plans";
-import PlanItem from "./PLanItem";
+import PlanItem from "./PlanItem";
 
 const Plans = () => {
   return (
-    <section className="-mt-78 relative z-10 md:mt-141 flex max-w-screen-1.5lg flex-col md:px-24 1.5lg:ml-auto 1.5lg:mr-auto 1.5lg:mt-149 xl:px-0">
+    <section className="relative z-10 -mt-78 flex max-w-screen-1.5lg flex-col md:mt-141 md:px-24 1.5lg:ml-auto 1.5lg:mr-auto 1.5lg:mt-149 xl:px-0">
       <div className="h-1 w-150 self-center bg-grayish-blue 1.5lg:self-start" />
       <h2 className="mt-39 text-center font-dm-serif-display text-46 leading-50 text-very-dark-violet 1.5lg:mt-55 1.5lg:text-left 1.5lg:text-70 1.5lg:leading-80">
         We’re different
